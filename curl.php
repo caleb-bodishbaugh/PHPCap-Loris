2a2c542ddefb7d2949c93c12c617db6a98807a2e
caleb-bodishbaugh <cbb3jn@virginia.edu>
2022-02-07 12:19:09 -0500

    wrote functions to get jwt token and using it to make other requests

--- curl.php
+++ curl.php
@@ -6,22 +6,72 @@ require_once("vendor/autoload.php");
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+$dotenv->required('LORIS_USERNAME', 'LORIS_PASSWORD', 'LORIS_API_URL')->notEmpty();
 
-$requestUri = $_ENV['LORIS_API_URL'] . "login";
+if (getenv('LORIS_JWT_TOKEN') === false) {
+        echo("Token not found. Retrieving with given credentials.\n");
+        get_loris_token();
+} else {
+        echo(getenv('LORIS_JWT_TOKEN') . "\n");
+        echo("Token already exists.\n");
+        echo("Here is your token: " . getenv('LORIS_JWT_TOKEN'));
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_URL, $requestUri);
-curl_setopt($ch, CURLOPT_SSH_COMPRESSION, true);
-curl_setopt_array($ch, [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_URL => $requestUri
-]);
-$result = curl_exec($ch);
-echo($result);
-$data = json_decode($result);
+}
 
-curl_close($ch);
+get_project('loris');
 
-echo($data);
+function get_project($project_name) {
+        $requestUri = $_ENV['LORIS_API_URL'] . "projects/" . $project_name;
+
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_URL => $requestUri,
+                CURLOPT_SSH_COMPRESSION => true,
+                CURLOPT_HTTPHEADER => array('Accept: application/json',
+                                            'Authorization: Bearer ' . getenv('LORIS_JWT_TOKEN')),
+        ]);
+        $result = curl_exec($ch);
+        echo($result . "\n");
+}
+
+function get_projects() {
+        $requestUri = $_ENV['LORIS_API_URL'] . "projects";
+
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_URL => $requestUri,
+                CURLOPT_SSH_COMPRESSION => true,
+                CURLOPT_HTTPHEADER => array('Accept: application/json',
+                                            'Authorization: Bearer ' . getenv('LORIS_JWT_TOKEN')),
+        ]);
+        $result = curl_exec($ch);
+        echo($result . "\n");
+}
+
+function get_loris_token() {
+        $requestUri = $_ENV['LORIS_API_URL'] . "login";
+        $login = array('username' => $_ENV['LORIS_USERNAME'],
+                'password' => $_ENV['LORIS_PASSWORD']);
+        $login_json = json_encode($login);
+
+
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_URL => $requestUri,
+                CURLOPT_POST => true,
+                CURLOPT_SSH_COMPRESSION => true,
+                CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+                CURLOPT_POSTFIELDS => $login_json,
+        ]);
+        $result = curl_exec($ch);
+        $token = json_decode($result);
+
+        echo($token->token . "\n");
+
+        putenv("LORIS_JWT_TOKEN=$token->token");
+
+        curl_close($ch);
+}
